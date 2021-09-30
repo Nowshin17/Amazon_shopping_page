e8a73f1f4b1b36f2fa904cbaa81f0fdd0c1ac8f0
Nowshin Chowdhury <80466283+Nowshin17@users.noreply.github.com>
2021-10-01 03:50:08 +0600

    Create Topbar.js

---
+++ src/topbar/Topbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from './topbar.module.css';
+
+const Topbar = (props) =>
+{
+    return(
+        <div className={classes.topbar}>
+
+            <img src = "https://www.freepnglogos.com/uploads/amazon-png-logo-vector/amazon-png-logo-vector-1.png" alt = "amazon logo" ></img>   
+        </div>
+
+    )
+
+
+}
+
+export default Topbar
