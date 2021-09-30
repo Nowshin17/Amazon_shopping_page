248c60c198827a31086b371f9555436448834153
Nowshin Chowdhury <80466283+Nowshin17@users.noreply.github.com>
2021-10-01 03:49:52 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import React,{Component} from 'react';
+import classes from './App.module.css';
+import ProductData from './Utils/ProductData';
+import ProductDetails from './productDetails/productDetails';
+import ProductPreview from './productPreview/productPreview';
+import Topbar from './topbar/Topbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+  // fatching the object from ProductData.js
+  state = {
+    productData: ProductData,
+    currentPreviewImagePos: 0,
+    currentSelectedFeature: 0,
+  }
+
+
+  onColorOptionClick = (pos) => {
+    // const updatedPreviewImage = this.state.productData.colorOptions[pos].imageUrl
+    this.setState({currentPreviewImagePos: pos});
+  }
+
+  onFeatureItemClick = (pos) =>{
+    let updatedState = false;
+    if(pos === 1)
+    {
+      updatedState = true;
+    }
+    this.setState({currentSelectedFeature: pos});
+
+  }
+  render(){
+
+    return (
+      <div className="App">
+  
+        {/* navebar */}
+        <header className="App-header">
+          <nav className = {classes.topbar}>
+            <Topbar />
+          </nav>
+        </header>
+         
+       
+  
+  
+        <div className={classes.mainContainer}>
+  
+            <div className = {classes.productPreview}>
+              <ProductPreview currentPreviewImage = {this.state.productData.colorOptions[this.state.currentPreviewImagePos].imageUrl}
+              currentSelectedFeature={this.state.currentSelectedFeature}  />
+            </div>
+  
+            <div className = {classes.ProductData}>
+              <ProductDetails data = {this.state.productData} onColorOptionClick ={this.onColorOptionClick} currentPreviewImagePos={this.state.currentPreviewImagePos} onFeatureItemClick={this.onFeatureItemClick} currentSelectedFeature = {this.state.currentSelectedFeature} />
+            </div>
+  
+        </div>
+  
+  
+  
+      </div>
+    );
+
+  }
+
 }
 
 export default App;
